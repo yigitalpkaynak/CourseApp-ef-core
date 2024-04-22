098579eef384770d552f8d4f9f836b32b073db32
yigitalpkaynak <manwithtuned@gmail.com>
2024-04-22 11:39:07 +0300

    first commit

---
+++ Data/KursKayit.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace efcoreApp.Data
+{
+    public class KursKayit
+    {   
+        [Key]
+        public int KayitId { get; set; }
+        public int OgrenciId { get; set; }
+        public Ogrenci Ogrenci { get; set; } = null!;
+        public int KursId { get; set; }
+        public Kurs Kurs { get; set; } = null!;
+
+        public DateTime KursKayitTarihi { get; set; }
+    }
+}
